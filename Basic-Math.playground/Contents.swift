b714090d2768d183e3ec66c3766c15ae56e412ee
Israel Manzo <israman814@gmail.com>
2018-01-02 10:33:44 -0500

    Adding a new playground

---
+++ Basic-Math.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Math playground
+// Este es un lugar para las matematicas
+
+// Sum
+2 + 3
+2 - 3
+2 * 3
+2 / 3
+
+var number1 = 2
+var number2 = 3
+
+number1 / number2
