1afda7501b0b8f6fd8b92f57699b07b10844f7d3
Israel Manzo <israman814@gmail.com>
2018-01-02 11:39:35 -0500

    Updating calculator

--- Simple-math-calculator.playground/Contents.swift
+++ Simple-math-calculator.playground/Contents.swift
@@ -2,3 +2,22 @@
 
 import UIKit
 
+// We will use two cases with if and switch statement
+
+// Simple Caculator with switch statement
+func simpleCalculator(x: Float, y: Float, operand: String)-> Float {
+    var result:Float = 0.0
+    switch operand {
+    case "+":
+        result = x + y
+    case "-":
+        result = x - y
+    case "*":
+        result = x * y
+    case "/":
+        result = x / y
+    default:
+        break
+    }
+    return result
+}
