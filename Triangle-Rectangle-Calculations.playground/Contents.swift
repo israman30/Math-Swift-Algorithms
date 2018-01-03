28dc9d651a79424a7135d40d0752afb51fd72156
Israel Manzo <israman814@gmail.com>
2018-01-03 13:20:04 -0500

    Adding alg function

--- Triangle-Rectangle-Calculations.playground/Contents.swift
+++ Triangle-Rectangle-Calculations.playground/Contents.swift
@@ -2,4 +2,37 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+//We will calculate the triangle rectangle
+
+/*
+ 
+    **
+    *   *
+  A *       *  C
+    *           *
+    *               *
+    *******************
+            B
+ A** = B** + C**
+       __________
+ A = \/ B** + C**
+ */
+
+
+
+// TRIANGLE RECTANGLE - PITAGORAS THEOREM
+func triangleRectangle(hyp: Float, cat1: Float, cat2: Float)-> Float {
+    var result: Float = 0.0
+    if cat1 == 0 {
+        result = (pow(hyp,2) - pow(cat2,2)).squareRoot()
+    } else if cat2 == 0 {
+        result = (pow(hyp,2) - pow(cat1,2)).squareRoot()
+    } else if hyp == 0 {
+        result = pow(cat1,2) + pow(cat2,2)
+    }
+    return result
+}
+triangleRectangle(hyp: 8, cat1: 0, cat2: 5)
+
+
