d982180a2f1b9f91280109f601fb5f9b12ade3ac
Israel Manzo <israelmanzo814@gmail.com>
2019-07-11 12:45:25 -0400

    Added insert sort on sorting playground

--- SortingElements.playground/Contents.swift
+++ SortingElements.playground/Contents.swift
@@ -1,5 +1,38 @@
 import UIKit
 
+/*
+ - INSERT SORT
+ 
+ - Insert sort gets a pinter and insert the pointer if this one is less than the element compared
+ [2,5,4,6,0,1,3]
+ [2, 5|, 4, 6, 0, 1 ,3]
+ [2, 5, 4|, 6, 0, 1 ,3] -> 5 > 4 = insert 4 before 5
+ [2, 4|, 5, 6, 0, 1 ,3]
+ [2, 4, 5|, 6, 0, 1 ,3]
+ [2, 4, 5, 6|, 0, 1 ,3]
+ [2, 4, 5, 6, 0|, 1 ,3] -> 6 > 0 = insert 0 before 6
+ [2, 4, 5, 0|, 6, 1 ,3] -> 5 > 0 = insert 0 before 5
+ [2, 4, 0|, 5, 6, 1 ,3] -> 4 > 0 = insert 0 before 4
+ [2, 0|, 4, 5, 6, 1 ,3] -> 2 > 0 = insert 0 before 2
+ [0|, 2, 4, 5, 6, 1 ,3] -> pointer will keep looping depending on the condition
+ [0, 2, 4, 5, 6, 1| ,3] -> pointer will do as same zero, will keep inserting till get less than the minimun element
+ 
+ */
+
+func insertSort(array: [Int]) -> [Int] {
+    var a = array
+    for i in 0..<a.count {
+        var j = i
+        while j > 0 && a[j] < a[j - 1] {
+            a.swapAt(j - 1, j)
+            j -= 1
+        }
+    }
+    return a
+}
+
+
+
 // - MERGE SORT
 
 /*
@@ -55,5 +88,11 @@ func sort(left: [Int], right: [Int]) -> [Int] {
     }
     return mergedArray + left + right
 }
+
+
+
+
+
+
 
 
