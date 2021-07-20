8a826f4caf7ade0f7c1ed92e8f456f484cfeb4ca
israman <israelmanzo814@gmail.com>
2021-07-20 14:02:06 -0400

    updated sum of two with search complement of sum method

--- FindSumOfTwoNumbers.playground/Contents.swift
+++ FindSumOfTwoNumbers.playground/Contents.swift
@@ -18,7 +18,7 @@ let numbers = [1, 2, 3, 4]
  sum[7] == target[7] -> return true
  **/
 
-func sumOfTwoNumbers(numbers: [Int], target: Int) -> Bool {
+func linearSumOfTwoNumbers(numbers: [Int], target: Int) -> Bool {
     var first = 0
     var last = numbers.count - 1
     while first < last {
@@ -35,4 +35,56 @@ func sumOfTwoNumbers(numbers: [Int], target: Int) -> Bool {
     return false
 }
 
-sumOfTwoNumbers(numbers: numbers, target: 2)
+
+/// Finding the complement number of the sum of 7
+/*
+ [:]
+ []
+ **/
+/// O(N)
+func sumOfTwoNumber(numbers: [Int], target: Int) -> Bool {
+    var dict = [Int:Int]()
+    for (index, value) in numbers.enumerated() {
+        let complement = target - value
+        if dict[complement] != nil {
+            return true
+        }
+        dict[value] = index
+    }
+    return false
+}
+
+/// Returnning the two number using the complement
+
+/*
+ [:]
+ Find teh sum of 7
+ [1, 3, 2, 3, 4, 4]
+ Look in the dicy for the complement
+ complement = 7 - 1 -> dict is empty
+ -> [1:0]
+ complement = 7 - 3
+ -> [1:0, 3:1]
+ complement = 7 - 2
+ -> [1:0, 3:1, 2:2]
+ complement = 7 - 3
+ -> [1:0, 3:1, 2:2, 3:3]
+ complement = 7 - 4
+ -> [1:0, 3:1, 2:2, 3:3, 4:4] at this point complement exist
+ 
+ **/
+func sumOfTwoNumberN(numbers: [Int], target: Int) -> [Int]? {
+    var dict = [Int:Int]()
+    for (index, value) in numbers.enumerated() {
+        let complement = target - value
+        if let complementIndex = dict[complement] {
+            return [complementIndex, index]
+        }
+        dict[value] = index
+    }
+    return nil
+}
+//sumOfTwoNumber(numbers: numbers, target: 11)
+
+sumOfTwoNumberN(numbers: numbers, target: 7)
+
